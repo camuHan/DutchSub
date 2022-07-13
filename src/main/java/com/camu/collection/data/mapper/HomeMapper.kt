01f22cae064a04a354dabe5beaf2ef7fce276430
한상혁 <mudaya3@gmail.com>
2022-07-14 01:20:33 +0900

    * GetDutchList 적용 * Coroutine 적용 (viewModelScope)

---
+++ src/main/java/com/camu/collection/data/mapper/HomeMapper.kt
@@ -0,0 +1,57 @@
+package com.camu.collection.data.mapper
+
+import com.camu.collection.data.model.DutchInfoDbEntity
+import com.camu.collection.domain.model.DutchInfo
+
+
+fun mapperToDutchInfoList(dutchInfoEntityList: List<DutchInfoDbEntity>): List<DutchInfo> {
+    return dutchInfoEntityList.toList().map { dutchInfoEntity ->
+        mapperToDutchInfo(dutchInfoEntity)
+    }
+}
+
+fun mapperToDutchInfo(dutchInfoEntity: DutchInfoDbEntity): DutchInfo {
+    return DutchInfo(
+        userId = dutchInfoEntity.userId,
+        dutchId = dutchInfoEntity.dutchId,
+        name = dutchInfoEntity.name,
+        title = dutchInfoEntity.title,
+        contents = dutchInfoEntity.contents,
+        contentsList = dutchInfoEntity.contentsList as ArrayList<String>,
+        location = dutchInfoEntity.location,
+        createdTime = dutchInfoEntity.createdTime,
+        modifiedTime = dutchInfoEntity.modifiedTime,
+        photoUrl = dutchInfoEntity.photoUrl,
+        likeCount = dutchInfoEntity.likeCount,
+        commentCount = dutchInfoEntity.commentCount,
+        viewCount = dutchInfoEntity.viewCount,
+        password = dutchInfoEntity.password,
+        locked = dutchInfoEntity.locked
+    )
+}
+
+fun mapperToDutchEntityList(dutchInfoList: List<DutchInfo>): List<DutchInfoDbEntity> {
+    return dutchInfoList.toList().map { dutchInfo ->
+        mapperToDutchEntity(dutchInfo)
+    }
+}
+
+fun mapperToDutchEntity(dutchInfo: DutchInfo): DutchInfoDbEntity {
+    return DutchInfoDbEntity(
+        userId = dutchInfo.userId,
+        dutchId = dutchInfo.dutchId,
+        name = dutchInfo.name,
+        title = dutchInfo.title,
+        contents = dutchInfo.contents,
+        contentsList = dutchInfo.contentsList,
+        location = dutchInfo.location,
+        createdTime = dutchInfo.createdTime,
+        modifiedTime = dutchInfo.modifiedTime,
+        photoUrl = dutchInfo.photoUrl,
+        likeCount = dutchInfo.likeCount,
+        commentCount = dutchInfo.commentCount,
+        viewCount = dutchInfo.viewCount,
+        password = dutchInfo.password,
+        locked = dutchInfo.locked
+    )
+}
--- src/main/java/com/camu/collection/data/repository/DutchRepositoryImpl.kt
+++ src/main/java/com/camu/collection/data/repository/DutchRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.camu.collection.data.repository
 
 import com.camu.collection.data.db.HomeLocalDutchDAO
+import com.camu.collection.data.mapper.mapperToDutchInfoList
 import com.camu.collection.domain.model.DutchInfo
 import com.camu.collection.domain.repository.DutchRepository
 
@@ -9,9 +10,9 @@ import javax.inject.Inject
 class DutchRepositoryImpl @Inject constructor(
     private val dutchDAO: HomeLocalDutchDAO
 ) : DutchRepository {
-    override suspend fun getDutchList() : List<DutchInfo>? {
-//        return mapping.mapper(dutchDAO.getAll())
-        val test = ArrayList<DutchInfo>()
-        return test
+    override suspend fun getDutchList() : List<DutchInfo> {
+        return mapperToDutchInfoList(dutchDAO.getAll())
+//        val test = ArrayList<DutchInfo>()
+//        return test
     }
 }
